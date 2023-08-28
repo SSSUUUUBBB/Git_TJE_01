69fc8199afd277ce9e5f6bc00f8f227a1667ddf1
Aster1 <gangjinsu4@gmail.com>
2023-08-28 16:30:26 +0900

    team4 DAO,DTO,Service,JDBConnection settings

--- JavaFX_Board/src/module-info.java
+++ JavaFX_Board/src/module-info.java
@@ -2,8 +2,7 @@ module JavaFX_Board {
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 	
-	opens application to javafx.graphics, javafx.fxml;
-	opens application.Controller to javafx.fxml;
-	opens application.DTO to javafx.base;
+	opens application to javafx.base, javafx.graphics;
 }
